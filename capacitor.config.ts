75b4e25dc89180e0d7590545fb5a9371ec081b83
irfan7o <kartolo727@gmail.com>
2023-12-01 13:45:39 +0800

    Biorhythm Calculator

---
+++ capacitor.config.ts
@@ -0,0 +1,14 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.example.biorhythms',
+  appName: 'Biorhythms',
+  webDir: 'build',
+  bundledWebRuntime: false,
+  server: {
+    url: 'http://192.168.1.120:3000',
+    cleartext: true,
+  },
+};
+
+export default config;
